425c9f9d2f7960538ebe33f475a4b880ee96bf5c
Alif Al Ahad <alifalahadxi@gmail.com>
2025-12-18 12:30:23 +0600

    check

--- Min_Cut.cpp
+++ Min_Cut.cpp
@@ -1,7 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+struct Edge{
+    int u,v,w;
+};
 int main()
 {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
+    int n;cin>>n;
+    int m;
+    cin>>m;
+    vector<Edge>E;
+    for(int i=0;i<m;i++){
+        //1 indexed
+        int a,b;
+        cin>>a>>b;
+        Edge e;
+        e.u=a;
+        e.v=b;
+        e.w=1;
+        E.push_back(e);
+    }
+
+    while(E.size()>1){
+        //1 indexed
+        n++;
+        int rnd=(rand() %(E.size())); 
+        int a,b;
+        a=E[rnd].u;
+        b=E[rnd].v;
+        //n
+        auto e=E[rnd];
+        E.erase(E.begin()+rnd);
+
+        for(int i=0;i<E.size();i++){
+            auto tmp=E[i];
+            if(tmp.u==a|| tmp.v==a){
+                if(tmp.v==a)swap(tmp.v,tmp.u);
+                //a=tmp.u;
+                int tb=tmp.v;
+                int wt=tmp.w;
+                //find
+                int toAdd=0;
+                for(int i=0;i<E.size();i++){
+                    auto news=E[i];
+                    if((news.u==n&&news.v==tb)||(news.v==n&&news.v==tb)){
+                        toAdd=news.w;
+                        break;
+                    }
+
+
+                }
+                Edge newe;
+                newe.w=n;
+                newe.v=tb;
+                newe.w=wt+toAdd;
+                E.push_back(newe);
+
+
+            }
+            else if(tmp.u==b||tmp.v==b){
+                if(tmp.u==b)swap(tmp.u,tmp.v);
+                int ta=tmp.u;
+                int wt=tmp.w;
+                 //find
+                int toAdd=0;
+                for(int i=0;i<E.size();i++){
+                    auto news=E[i];
+                    if((news.u==n&&news.v==ta)||(news.v==n&&news.v==ta)){
+                        toAdd=news.w;
+                        break;
+                    }
+
+
+                }
+                Edge newe;
+                newe.w=n;
+                newe.v=ta;
+                newe.w=wt+toAdd;
+                E.push_back(newe);
+                
+                
+
+
+            }
+        }
+
+
+
+
+    }
+    cout<<E[0].w<<endl;
 }
